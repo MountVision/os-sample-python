550c0566406ad32c34557c1f1eb100fc56e131f3
MountVision <eliad.sason@gmail.com>
2017-12-27 13:02:56 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
 <<<<<<< HEAD
-    return "Hello Eliad 222"
+    return "Hello Eliad Sason"
 >>>>>>> d54bea9ca958ded33051d510835afce44e1262fc
 
 if __name__ == "__main__":
