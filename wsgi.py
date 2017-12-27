bbebfa1c83bd58598141ffc7bcb998d223a372d0
MountVision <eliad.sason@gmail.com>
2017-12-27 16:02:13 +0200

    Change greeting.

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
 <<<<<<< HEAD
-    return "Hello Eliad Sason "
+    return "Hello Eliad Sason 11111 "
 >>>>>>> d54bea9ca958ded33051d510835afce44e1262fc
 
 if __name__ == "__main__":
